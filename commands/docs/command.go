c5000567f6e3af919e00f6b8cac26eca1e3e4fc7
fuyibing <websearch@163.com>
2021-02-18 23:28:08 +0800

    no message

--- commands/docs/command.go
+++ commands/docs/command.go
@@ -20,9 +20,21 @@ func New() base.CommandInterface {
 	o.Initialize()
 	o.SetName("docs")
 	o.SetDescription("Create application documents")
+	// append options
+	o.AddOption(
+		base.NewOption("path", base.OptionModeOptional, base.OptionValueModeString).
+			SetShortName("p").
+			SetDefaultValue("./app").
+			SetDescription("Source file directory (default: ./app)"),
+		base.NewOption("target", base.OptionModeOptional, base.OptionValueModeString).
+			SetShortName("t").
+			SetDefaultValue("./docs").
+			SetDescription("Created file save to (default: ./docs)"),
+	)
 	return o
 }
 
+// Run command.
 func (o *command) Run(manager base.ManagerInterface, args []string) error {
 	if err := o.ParseArguments(args); err != nil {
 		return err
